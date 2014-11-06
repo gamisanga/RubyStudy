47a401bbaf60a377d1e0aaaaedc4dea1a068d4ab
gamisanga <wing.tek.zero@gmail.com>
2014-11-06 17:44:19 +0900

    03-00完了

---
+++ app/controllers/welcom_controller.rb
@@ -0,0 +1,17 @@
+class WelcomController < ApplicationController
+  def index
+  end
+
+  def tos
+  end
+
+  def policy
+  end
+
+  def about
+  end
+
+  def test
+  	redirect_to '/', :notice => 'notice!!'
+  end
+end
